06ead1b690a760b6e3edf5b232271caebd8d374d
DongUk <mywayx1205@naver.com>
2023-08-09 17:33:04 +0900

    Update calendar.js

--- mymemories/src/components/calendar/calendar.js
+++ mymemories/src/components/calendar/calendar.js
@@ -3,19 +3,27 @@ import './style.css';
 
 function Calendar() {
   const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  //요일 표시
 
   const [currentDate, setCurrentDate] = useState(new Date());
+  //현재 날짜 상태?
 
   const getDaysInMonth = (year, month) => new Date(year, month + 1, 0).getDate();
+  //현재 달의 총 일수
+  
   const getFirstDayOfWeek = (year, month) => new Date(year, month, 1).getDay();
+  //현재달의 첫번째 요일(0은 일요일 ~ 6은 토요일)
 
+  
   const handlePrevMonth = () => {
     setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1));
-  };
+  }; //전달로 가는 버튼
 
   const handleNextMonth = () => {
     setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1));
-  };
+  }; //다음달로 가는 버튼
+
+
 
   const renderCalendar = () => {
     const daysInMonth = getDaysInMonth(currentDate.getFullYear(), currentDate.getMonth());
@@ -23,10 +31,11 @@ function Calendar() {
 
     const calendarDays = [];
 
+    //이전달의 빈 날짜 추가
     for (let i = 0; i < firstDayOfWeek; i++) {
       calendarDays.push(<div className="empty-day" key={`empty-${i}`}></div>);
     }
-
+    //현재 달 날짜 추가
     for (let day = 1; day <= daysInMonth; day++) {
       calendarDays.push(<div className="calendar-day" key={`day-${day}`}>{day}</div>);
     }
@@ -36,7 +45,6 @@ function Calendar() {
 
   return (
     <div className="App">
-      
       <div className="calendar">
         <div className="header">
           <button onClick={handlePrevMonth}>&lt;</button>
